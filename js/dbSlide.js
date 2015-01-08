7993d51c41a9412bc28e4285e68212d06a6f856c
moovin <movin@kth.se>
2015-01-08 16:31:35 +0100

    dynamisk html för mat

--- js/dbSlide.js
+++ js/dbSlide.js
@@ -1,3 +1,25 @@
+function makeHTML(jsonList) {
+	var obj;
+	var list = jsonList;
+	for (obj in list) {
+		console.log("inne i loop");
+		console.log(list[obj]);
+		var h;
+		if (list[obj].type == "food") {
+			h = "<div class='slides'><div class='row'><div class='col-md-12'>" +
+				"<u><h1>Matsedel</h1></u></div></div><div class='content'><div class='row'>" +
+				"<div class='col-md-1'></div><div class='col-md-5'><h3 class='bold'> Lunch: </h3>" +
+				"<h3 class='lessMargin'> " + list[obj].text1 + " </h3><div class='col-md-1'></div>" +
+				"<h3 class='bold'> Middag: </h3><h3 class='lessMargin'> " + list[obj].text2 + " </h3></div>" +
+				"<div class='col-md-5'><img class='medium-img' src='img/" + list[obj].imgURL + "'/></div>" +
+				"<div class='col-md-1'></div></div></div></div>";
+		} else {
+			console.log("annan typ än food");
+		}
+		$("#slideshow").append(h);
+	}
+}
+
 function getSlides(str) {
 	console.log('inne i getSlides');
 	if (str == "") {
@@ -15,6 +37,15 @@ function getSlides(str) {
 		xmlhttp.onreadystatechange = function() {
 			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 				console.log(xmlhttp.responseText);
+				var jsonList = [{
+					"id": "1",
+					"type": "food",
+					"imgURL": "matikon.png",
+					"text1": "mumsgott",
+					"text2": "testis"
+				}];
+				console.log(jsonList.length);
+				makeHTML(jsonList);
 			}
 		}
 		xmlhttp.open("GET", "getSlides.php?q=" + str, true);
@@ -22,6 +53,14 @@ function getSlides(str) {
 	}
 }
 
+function getDates() {
+	var date = "2015-01-04";
+	getSlides(date);
+}
+
+
+
 $(document).ready(function() {
 	getSlides("hej");
+	//getDates();
 });
